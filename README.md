0368e99b5bd9b5a9b13f88a5a466446b63e0ba3e
João Paulo Barraca <jpbarraca@gmail.com>
2020-11-12 11:40:39 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ctfmate
+
+A "Fork" of the CTFMate "application" created by Eduardo Vela ( [original tweet](https://twitter.com/sirdarckcat/status/1008327016087801856)) just to track fixes and improvements (if any).
+
---
+++ public/js/config.js
@@ -0,0 +1,11 @@
+// Change this to your own!
+// https://firebase.google.com/docs/database/web/start
+var config = {
+	apiKey: "",
+	authDomain: "",
+	databaseURL: "",
+	projectId: "",
+	storageBucket: "",
+	messagingSenderId: "",
+	appId: ""
+}
